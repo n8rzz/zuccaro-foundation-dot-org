4783fddcd883fc2a6883d941fda1f7609b176c96
Nate Geslin <teamtomkins23@gmail.com>
2024-12-27 13:35:41 -0600

    Initialize!

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,39 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `zuccaro-foundation-dot-org`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://zuccarofoundation.org`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // Only include trackingId if it exists
+        trackingIds: process.env.GATSBY_GOOGLE_ANALYTICS_ID ? [process.env.GATSBY_GOOGLE_ANALYTICS_ID] : [],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
